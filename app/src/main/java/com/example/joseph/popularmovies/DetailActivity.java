8ecf2c8b21c747416860325aa087459deb017cda
Xialong Deng <xdeng9@gmail.com>
2016-06-27 15:04:55 -0700

    Fix Add to favorite button

--- app/src/main/java/com/example/joseph/popularmovies/DetailActivity.java
+++ app/src/main/java/com/example/joseph/popularmovies/DetailActivity.java
@@ -33,6 +33,7 @@ public class DetailActivity extends AppCompatActivity {
     LinearLayout trailerLayout;
     LinearLayout reviewLayout;
     Movie mMovie;
+    Button favbtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,6 +55,7 @@ public class DetailActivity extends AppCompatActivity {
         ((TextView) findViewById(R.id.movie_rating_textview)).setText(mMovie.getUserRating());
         ((TextView) findViewById(R.id.movie_release_date_textview)).setText(mMovie.getReleaseDate());
         ((TextView) findViewById(R.id.movie_plot_summary_textview)).setText(mMovie.getPlotSummary());
+        favbtn = (Button) findViewById(R.id.fav_btn);
         setTitle(mMovie.getMovieTitle());
         ImageView movieImage = ((ImageView) findViewById(R.id.movie_poster_imageview));
         Picasso.with(mContext).load(mMovie.getImageUrl()).into(movieImage);
@@ -76,8 +78,8 @@ public class DetailActivity extends AppCompatActivity {
     }
 
     public void addMovieToFavorite(View v) {
-        MovieDbHelper dbHelper = new MovieDbHelper(getApplicationContext());
-        SQLiteDatabase db = dbHelper.getWritableDatabase();
+//        MovieDbHelper dbHelper = new MovieDbHelper(getApplicationContext());
+//        SQLiteDatabase db = dbHelper.getWritableDatabase();
         ContentValues values = new ContentValues();
         values.put(MovieContract.MovieEntry.COLUMN_MOVIE_ID, mMovie.getMovieId());
         values.put(MovieContract.MovieEntry.COLUMN_MOVIE_NAME, mMovie.getMovieTitle());
@@ -86,9 +88,11 @@ public class DetailActivity extends AppCompatActivity {
         values.put(MovieContract.MovieEntry.COLUMN_MOVIE_POSETER_URL, mMovie.getImageUrl());
         values.put(MovieContract.MovieEntry.COLUMN_MOVIE_OVERVIEW, mMovie.getPlotSummary());
         values.put(MovieContract.MovieEntry.COLUMN_MOVIE_HEADER_URL, mMovie.getBackdrop());
-        db.insert(MovieContract.MovieEntry.TABLE_NAME, null, values);
+       // db.insert(MovieContract.MovieEntry.TABLE_NAME, null, values);
+       // db.close();
 
-        db.close();
+        getApplicationContext().getContentResolver().insert(MovieContract.MovieEntry.CONTENT_URI, values);
+        favbtn.setText("Remove from favorite");
         Toast.makeText(getApplicationContext(), "Movie added to favorite!", Toast.LENGTH_SHORT).show();
     }
 
